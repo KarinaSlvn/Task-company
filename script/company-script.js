8dfe04c124ec5e4978a40e1ba38fef81f3c2b163
Karina <karyna.selivanova@nure.ua>
2018-04-25 00:58:43 +0300

    created logic for sorting partners

--- script/company-script.js
+++ script/company-script.js
@@ -2,6 +2,17 @@
  * Created by Karina on 22.04.2018.
  */
 "use strict";
+let arrCompanyPartners = [];
+let sortedPartnersValue = [];
+
+const SortingTypes = {
+    VALUE_UP: 'VALUE_UP',
+    VALUE_DOWN: 'VALUE_DOWN',
+    NAME_DOWN: 'NAME_DOWN',
+    NAME_UP: 'NAME_UP'
+};
+
+let sortingType = SortingTypes.VALUE_DOWN;
 /*
  google.charts.load('current', {packages: ['corechart']});
  google.charts.setOnLoadCallback(drawChart);
@@ -38,7 +49,7 @@ const asyncHider = () => {
     const timer = setTimeout(() => {
         hideLoader();
         clearTimeout(timer)
-    }, 200)
+    }, 1)
 };
 
 let countries = [];
@@ -56,29 +67,64 @@ function arrAllCompanies(arrCompanies) {
         companies.push(company.name);
     });
     for (let i = 0; i <= companies.length - 1; i++) {
-        let div = $("<a class='each-companies' href='#'></a>");
-        div.append(companies[i]);
-        $(".list-all-companies").after(div);
+        let a = $("<a class='each-companies' href='#'></a>");
+        a.append(companies[i]);
+        $(".list-all-companies").after(a);
 
     }
-    showCompanyPartners();
-}
-let arrCompanyPartners = [];
-
-function showCompanyPartners() {
     $(".each-companies").on("click", function () {
-        $(".company-partners").css("display", "flex");
-        const companyName = $(this).text();
-        arrCompanyPartners = _.find(companiesArr, ['name', companyName]).partners;
+        showCompanyPartners(this);
     })
-
+    // showCompanyPartners();
 }
-$(document).ready(
+
+function changeSortingType(type) {
+    sortingType = type
+}
+
+function getOrder(sortType) {
+    return sortType === SortingTypes.VALUE_DOWN || sortType === SortingTypes.NAME_DOWN ? 'asc' : 'desc'
+}
+
+function getSortBy(sortType) {
+    return sortType === SortingTypes.VALUE_DOWN || sortType === SortingTypes.VALUE_UP ? 'value' : 'name'
+}
+
+function getSortedArr(arr, sortType) {
+    return sortedPartnersValue = _.orderBy(arr, [getSortBy(sortType)], [getOrder(sortType)]);
+}
+function constructorPartnersBlock() {
+    $(".partner").remove();
+    getSortedArr(arrCompanyPartners, sortingType).forEach(item => {
+        console.log(item)
+        const companyPartner = $("<div class='partner'></div>");
+        $(".list-partner").after(companyPartner);
+        companyPartner.append(`${item.name} - ${item.value}%`);
+    })
+}
+
+function getCompanyPartners(_this) {
+    $(".company-partners").css("display", "flex");
+    const companyName = $(_this).text();
+    arrCompanyPartners = getPartnersInPercentage(_.find(companiesArr, ['name', companyName]).partners);
+}
+function getPartnersInPercentage(arr) {
+    const sumValue = _.sumBy(arr, 'value')
+    return arr.map(item => {
+        return {'name': item.name, 'value': Number((item.value / sumValue * 100).toFixed(1))}
+    })
+}
+
+function showCompanyPartners(_this) {
+    getCompanyPartners(_this);
+    constructorPartnersBlock();
+}
+
+function getCompanies() {
     $.ajax({
             type: "Get",
             url: "http://codeit.pro/codeitCandidates/serverFrontendTest/company/getList",
             success: res => {
-                console.log(res);
                 companiesArr = res.list;
                 parseCompany(companiesArr);
                 asyncHider();
@@ -90,15 +136,47 @@ $(document).ready(
 
         }
     )
-)
-$(document).ready(
+}
+
+function addEventListenerForArrows() {
+    const ArrowsSort = [
+        {
+            className: 'sort-value-down',
+            sort: SortingTypes.VALUE_DOWN
+        },
+        {
+            className: 'sort-value-up',
+            sort: SortingTypes.VALUE_UP
+        },
+        {
+            className: 'sort-name-down',
+            sort: SortingTypes.NAME_DOWN
+        },
+        {
+            className: 'sort-name-up',
+            sort: SortingTypes.NAME_UP
+        }
+    ];
+    ArrowsSort.forEach(arrow => {
+        $('.' + arrow.className).on("click", function () {
+            changeSortingType(arrow.sort)
+            constructorPartnersBlock()
+        })
+    })
+}
+function getNews() {
     $.ajax({
             type: "Get",
             url: "http://codeit.pro/codeitCandidates/serverFrontendTest/news/getList",
             success: res => {
-                console.log(res)
             }
         }
     )
-)
+}
+$(function () {
+    getCompanies();
+    getNews();
+    addEventListenerForArrows();
+})
+
 
